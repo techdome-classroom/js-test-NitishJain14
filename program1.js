55f3ee88d850a99d1bca7337db6182b65acfc879
Nitish Jain <nitishjain722@gmail.com>
2024-07-26 13:29:41 +0530

    Techdome test

--- program1.js
+++ program1.js
@@ -1,8 +1,43 @@
-const getTotalIsles = function (grid) {
+// program1.js
 
+function getTotalIsles(grid) {
+  if (!grid || grid.length === 0){
+    return 0;
+  } 
 
-  // write your code here
+  let totalIsles = 0;
+  const rows = grid.length;
+  const cols = grid[0].length;
 
-};
+  // Helper function to perform DFS on the grid
+  function dfs(i, j) {
+      // Check for out-of-bounds or water
+      if (i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] === 'W'){
+        return;
+      } 
+
+      // Mark the cell as visited by changing 'L' to 'W'
+      grid[i][j] = 'W';
+
+      // Explore all four directions
+      dfs(i - 1, j); 
+      dfs(i + 1, j); 
+      dfs(i, j - 1); 
+      dfs(i, j + 1); 
+  }
+
+  // Iterate over each cell in the grid
+  for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+          if (grid[i][j] === 'L') {
+              // Found an unvisited island part
+              totalIsles++;
+              dfs(i, j); // Perform DFS to mark the entire island
+          }
+      }
+  }
+
+  return totalIsles;
+}
 
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,22 @@
-const decodeTheRing = function (s, p) {
+const decodeTheRing = function(s, p) {
+  // Split the pattern by '*'
+  const parts = p.split('*');
+  let currIdx = 0;
 
-    // write your code here
+  for (let i = 0; i < parts.length; i++) {
+    const part = parts[i];
+    
+    let idx = s.indexOf(part, currIdx);
 
-  };
-  
-  module.exports = decodeTheRing;
+    if (idx === -1 || (i === 0 && idx !== 0 && p[0] !== '*') || (i === parts.length - 1 && idx + part.length !== s.length && p[p.length - 1] !== '*')) {
+      return false;
+    }
+
+    
+    currIdx = idx + part.length;
+  }
+
+  return true;
+};
+
+module.exports = decodeTheRing;
